6d29d78862c49a39bca906385d906920128958f2
Olly Monger <olivermonger123@gmail.com>
2023-02-24 23:23:04 +0000

    upload photos

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { posts } from "./data/posts";
 
 function App() {
     const planets = [
-        { name: "Mercury", color: "grey", image:"/mercury.png", distanceFromParent: 1, diameter: 10, orbitalPeriod: 88 },
+        { name: "Mercury", color: "none", image:"/mercury.png", distanceFromParent: 1.5, diameter: 10, orbitalPeriod: 88 },
         { name: "Venus", color: "yellow", image:"/venus.png", distanceFromParent: 3, diameter: 20, orbitalPeriod: 225 },
 
         { 
@@ -18,9 +18,9 @@ function App() {
             ] 
         },
 
-        { name: "Mars", color: "red", image:"/mars.png", distanceFromParent: 15, diameter: 40, orbitalPeriod: 687 },
-        { name: "Jupiter", color: "orange", image:"/jupiter.png", distanceFromParent: 25, diameter: 100, orbitalPeriod: 4333 },
-        { name: "Saturn", color: "brown", image:"/saturn.png", distanceFromParent: 35, diameter: 90, orbitalPeriod: 10759 },
+        { name: "Mars", color: "none", image:"/mars.png", distanceFromParent: 15, diameter: 40, orbitalPeriod: 687 },
+        { name: "Jupiter", color: "none", image:"/jupiter.png", distanceFromParent: 25, diameter: 100, orbitalPeriod: 4333 },
+        { name: "Saturn", color: "brown", distanceFromParent: 35, diameter: 90, orbitalPeriod: 10759 },
         { name: "Uranus", color: "lightblue", image:"/uranus.png", distanceFromParent: 45, diameter: 80, orbitalPeriod: 30687 },
         { name: "Neptune", color: "darkblue", image:"/neptune.png", distanceFromParent: 55, diameter: 70, orbitalPeriod: 60190 },
     ]
